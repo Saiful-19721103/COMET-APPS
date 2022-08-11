4393dafabdb0093a14e9c5e28a7212c603afd2cd
Saiful19721103 <saiful197583@gmail.com>
2022-08-11 23:29:50 +0600

    Upload and setup dashboard by [app,header,sidebar blade]

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,77 @@
+<!-- Sidebar -->
+<div class="sidebar" id="sidebar">
+                <div class="sidebar-inner slimscroll">
+					<div id="sidebar-menu" class="sidebar-menu">
+						<ul>
+							<li class="menu-title"> 
+								<span>Main</span>
+							</li>
+							<li class="active"> 
+								<a href="#"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+							</li>
+
+                            <li class="active"> 
+								<a href="#"><i class="fe fe-home"></i> <span>Slider</span></a>
+							</li>
+
+                            <li class="active"> 
+								<a href="#"><i class="fe fe-home"></i> <span>Testimonial</span></a>
+							</li>
+
+                            <li class="active"> 
+								<a href="#"><i class="fe fe-home"></i> <span>Clients</span></a>
+							</li>
+                        
+                            <!--Portfolio-->
+                            <li class="submenu">
+								<a href="#"><i class="fe fe-document"></i> <span> Admin Users</span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="invoice-report.html">Useres</a></li>
+                                    <li><a href="invoice-report.html">Role</a></li>
+                                    <li><a href="invoice-report.html">Permission</a></li>
+								</ul>
+							</li>
+                            <!--Portfolio-->
+
+                            <!--Team Section-->
+                            <li class="active"> 
+								<a href="#"><i class="fe fe-home"></i> <span>Our Team</span></a>
+							</li>
+                            <!--Team Section-->
+                            
+                            <!--Sub Menue-->
+							<li class="submenu">
+								<a href="#"><i class="fe fe-document"></i> <span> Posts</span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="invoice-report.html">All Posts</a></li>
+                                    <li><a href="invoice-report.html">Catagory</a></li>
+                                    <li><a href="invoice-report.html">Tags</a></li>
+								</ul>
+							</li>
+                            <!--Sub Menue-->
+
+                            <!--Admin Options-->
+                            <li class="menu-title"> 
+								<span>Admin Optiond</span>
+							</li>
+                            <li class="submenu">
+								<a href="#"><i class="fe fe-document"></i> <span> Admin Users</span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="invoice-report.html">Useres</a></li>
+                                    <li><a href="invoice-report.html">Role</a></li>
+                                    <li><a href="invoice-report.html">Permission</a></li>
+								</ul>
+							</li>
+                            <!--Admin Options-->
+
+                            <!-- Seetings-->
+                            <li class="active"> 
+								<a href="#"><i class="fe fe-home"></i> <span>Seetings</span></a>
+							</li>
+                            <!-- Seetings-->
+
+						</ul>
+					</div>
+                </div>
+            </div>
+			<!-- /Sidebar -->
