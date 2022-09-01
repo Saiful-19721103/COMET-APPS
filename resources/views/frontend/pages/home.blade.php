fbbb1c045bda2989fccb159904d2cbd63beed3ec
Saiful19721103 <saiful197583@gmail.com>
2022-09-01 09:34:07 +0600

    Slider makes Dynamic

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -6,34 +6,29 @@
 <section id="home">
  <!-- Home Slider-->
  <div id="home-slider" class="flexslider">
+
   <ul class="slides">
+   @php
+   $sliders=App\Models\Slider::latest()->get();
+   @endphp
+
+   @foreach($sliders as $slider)
    <li>
-    <img src="frontend/images/bg/1.jpg" alt="">
+    <img src="{{url ('storage/sliders/' . $slider->photo)}}" alt="">
     <div class="slide-wrap">
      <div class="slide-content">
       <div class="container">
-       <h1>Digital Power<span class="red-dot"></span></h1>
-       <h6>We are a small design studio from San Francisco.</h6>
+       <h1>{{$slider->title}}<span class="red-dot"></span></h1>
+       <h6>{{$slider->subtitle}}</h6>
        <p><a href="#" class="btn btn-light-out">Read More</a><a href="#" class="btn btn-color btn-full">Services</a>
        </p>
       </div>
      </div>
     </div>
    </li>
-   <li>
-    <img src="frontend/images/bg/2.jpg" alt="">
-    <div class="slide-wrap">
-     <div class="slide-content">
-      <div class="container">
-       <h1>We Are Comet<span class="red-dot"></span></h1>
-       <h6>Experts in web design and development.</h6>
-       <p><a href="#" class="btn btn-color">Explore</a><a href="#" class="btn btn-light-out">Join us</a>
-       </p>
-      </div>
-     </div>
-    </div>
-   </li>
+   @endforeach
   </ul>
+
  </div>
  <!-- End Home Slider-->
 </section>
